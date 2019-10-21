c635c8c044e31772480215e369353ca0fa4e0531
Parth <kparth2010@gmail.com>
2019-10-21 14:30:41 +0530

    init

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, url_for, flash, redirect  
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager
+from flask_login import UserMixin, current_user, login_user, logout_user, login_required
+import os
+
+
+app=Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_manager= LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User1.query.get(int(user_id))
+
+
+class User1(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    Department = db.Column(db.String(20))
+    Company = db.Column(db.String(20) )
+    Location = db.Column(db.String(20))
+    Mobile = db.Column(db.Integer)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password=db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return "People {0},{1}".format(self.username, self.email)
+
+
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')  
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User1(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User1.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data, force=True)
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))       
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
+
+@app.route("/product")
+@login_required
+def search():
+    return render_template('product.html', title='Products')
+
+if __name__== '__main__':
+    app.run(debug=True)
